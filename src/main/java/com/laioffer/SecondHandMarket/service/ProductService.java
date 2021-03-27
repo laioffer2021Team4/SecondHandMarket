71083dc7e9b1e187f64640fcd0bd1c6959e29ccf
Xinhao Song <songxinhao@hotmail.com>
2021-03-26 23:38:57 -0500

    Create Product Service class

---
+++ src/main/java/com/laioffer/SecondHandMarket/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.laioffer.SecondHandMarket.service;
+
+import com.laioffer.SecondHandMarket.dao.ProductDao;
+import com.laioffer.SecondHandMarket.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    public List<Product> getAllProducts() {
+        return productDao.getAllProducts();
+    }
+
+    public Product getProductById(int productId) {
+        return productDao.getProductById(productId);
+    }
+
+    public void deleteProduct(int productId) {
+        productDao.deleteProduct(productId);
+    }
+
+    public void addProduct(Product product) {
+        productDao.addProduct(product);
+    }
+
+    public void updateProduct(Product product) {
+        productDao.updateProduct(product);
+    }
+}
