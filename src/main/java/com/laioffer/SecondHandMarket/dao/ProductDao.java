e577a9f022a427aec29b165484dca36713434bf0
Xinhao Song <songxinhao@hotmail.com>
2021-03-26 23:38:33 -0500

    Create Product DAO class

---
+++ src/main/java/com/laioffer/SecondHandMarket/dao/ProductDao.java
@@ -0,0 +1,88 @@
+package com.laioffer.SecondHandMarket.dao;
+
+import com.laioffer.SecondHandMarket.entity.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductDao {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    public void addProduct(Product product) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save(product);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public void deleteProduct(int productId) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            Product product = session.get(Product.class, productId);
+            session.delete(product);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public void updateProduct(Product product) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.saveOrUpdate(product);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public Product getProductById(int productId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Product.class, productId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<>();
+        try (Session session = sessionFactory.openSession()) {
+            products = session.createCriteria(Product.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+}
