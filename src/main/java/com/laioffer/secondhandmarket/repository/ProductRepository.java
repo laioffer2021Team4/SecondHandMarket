c45251b63014dc7b992aa382ce9c4ec54c6822b2
Xinhao Song <songxinhao@hotmail.com>
2021-04-01 23:51:29 -0500

    Create repository interface for entity Product

---
+++ src/main/java/com/laioffer/secondhandmarket/repository/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.laioffer.secondhandmarket.repository;
+
+import com.laioffer.secondhandmarket.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findAll();
+
+    Product findProductById(int id);
+
+    void deleteProductById(int id);
+
+    Product save(Product product);
+
+    List<Product> findByDescriptionContaining(String keyword);
+}
