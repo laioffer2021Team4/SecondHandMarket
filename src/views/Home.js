0c15f2bfe1c99f79e151c87cd27fb6c1d3ff54da
OrangeCJN <orangecjn@gmail.com>
2021-04-15 21:33:16 -0400

    Added background picture and removed feeds on home page.

--- src/views/Home.js
+++ src/views/Home.js
@@ -19,53 +19,54 @@ class Home extends React.Component {
     super(props);
 
     this.state = {
-      // First list of posts.
-      PostsListOne: [
-        {
-          backgroundImage: require("../images/content-management/1.jpeg"),
-          category: "Business",
-          categoryTheme: "dark",
-          author: "Anna Kunis",
-          authorAvatar: require("../images/avatars/1.jpg"),
-          title: "Conduct at an replied removal an amongst",
-          body:
-            "However venture pursuit he am mr cordial. Forming musical am hearing studied be luckily. But in for determine what would see...",
-          date: "28 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/2.jpeg"),
-          category: "Travel",
-          categoryTheme: "info",
-          author: "James Jamerson",
-          authorAvatar: require("../images/avatars/2.jpg"),
-          title: "Off tears are day blind smile alone had ready",
-          body:
-            "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/3.jpeg"),
-          category: "Technology",
-          categoryTheme: "royal-blue",
-          author: "Jimmy Jackson",
-          authorAvatar: require("../images/avatars/2.jpg"),
-          title: "Difficult in delivered extensive at direction",
-          body:
-            "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/4.jpeg"),
-          category: "Business",
-          categoryTheme: "warning",
-          author: "John James",
-          authorAvatar: require("../images/avatars/3.jpg"),
-          title: "It so numerous if he may outlived disposal",
-          body:
-            "How but sons mrs lady when. Her especially are unpleasant out alteration continuing unreserved ready road market resolution...",
-          date: "29 February 2019"
-        }
-      ]
+      //First list of posts.
+      PostsListOne:[],
+      // PostsListOne: [
+      //   {
+      //     backgroundImage: require("../images/content-management/1.jpeg"),
+      //     category: "Business",
+      //     categoryTheme: "dark",
+      //     author: "Anna Kunis",
+      //     authorAvatar: require("../images/avatars/1.jpg"),
+      //     title: "Conduct at an replied removal an amongst",
+      //     body:
+      //       "However venture pursuit he am mr cordial. Forming musical am hearing studied be luckily. But in for determine what would see...",
+      //     date: "28 February 2019"
+      //   },
+      //   {
+      //     backgroundImage: require("../images/content-management/2.jpeg"),
+      //     category: "Travel",
+      //     categoryTheme: "info",
+      //     author: "James Jamerson",
+      //     authorAvatar: require("../images/avatars/2.jpg"),
+      //     title: "Off tears are day blind smile alone had ready",
+      //     body:
+      //       "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
+      //     date: "29 February 2019"
+      //   },
+      //   {
+      //     backgroundImage: require("../images/content-management/3.jpeg"),
+      //     category: "Technology",
+      //     categoryTheme: "royal-blue",
+      //     author: "Jimmy Jackson",
+      //     authorAvatar: require("../images/avatars/2.jpg"),
+      //     title: "Difficult in delivered extensive at direction",
+      //     body:
+      //       "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
+      //     date: "29 February 2019"
+      //   },
+      //   {
+      //     backgroundImage: require("../images/content-management/4.jpeg"),
+      //     category: "Business",
+      //     categoryTheme: "warning",
+      //     author: "John James",
+      //     authorAvatar: require("../images/avatars/3.jpg"),
+      //     title: "It so numerous if he may outlived disposal",
+      //     body:
+      //       "How but sons mrs lady when. Her especially are unpleasant out alteration continuing unreserved ready road market resolution...",
+      //     date: "29 February 2019"
+      //   }
+      // ]
 
 
     };
