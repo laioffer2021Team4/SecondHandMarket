2c5b3302583cd9714eded106137e41e28e95c795
YingYing <xin.yingying3303@gmail.com>
2021-04-18 16:18:42 -0700

    Add isAuth to decide what content to be render: button or contact info

--- src/views/UserInfo.js
+++ src/views/UserInfo.js
@@ -9,9 +9,18 @@ import {
   Button
 } from "shards-react";
 import user_avatar from '../images/avatars/user.png';
+import authService from "../auth/authService"
 
 class UserInfo extends Component {
+  state={isAuth: undefined};
 
+  componentDidMount() {
+    if(authService.getCurrentUser() && authService.getCurrentUser().accessToken ){
+      this.setState({isAuth: true});
+    } else{
+      this.setState({isAuth: false});
+    }
+  }
   // state = {
   //   userInfo: null
   // }
@@ -23,21 +32,26 @@ class UserInfo extends Component {
 
 
   render() {
-    const userInfo = this.props.userInfo;
+    const sellerInfo = this.props.userInfo;
+    const {isAuth} = this.state;
     return (
       <Card style={{maxWidth: "100%", height: "inherit"}}>
         <CardHeader>Here's Seller</CardHeader>
         <CardImg style={{display: "block",marginLeft:"auto", marginRight:"auto", maxWidth: "80%"}} src={user_avatar} />
         <CardBody>
-          <CardTitle>{userInfo.firstName} {userInfo.lastName}</CardTitle>
+          <CardTitle>{sellerInfo.firstName} {sellerInfo.lastName}</CardTitle>
           {
             // Object.keys(userInfo.address).map((key, i) => {
             //   return <div>{userInfo.address[key]}</div>
             // })
           }
-          <Button style={{position: "relative", top: "10px"}}>More Info &rarr;</Button>
+
+          {
+            isAuth? <p>{sellerInfo.useremail}</p> : <Button style={{position: "relative", top: "10px"} }> <a href={"/login"}>Contact Me</a></Button>
+          }
+
         </CardBody>
-        <CardFooter><a href={`mailto:${userInfo.useremail}`}>Contact Me</a></CardFooter>
+
       </Card>
     );
   }
